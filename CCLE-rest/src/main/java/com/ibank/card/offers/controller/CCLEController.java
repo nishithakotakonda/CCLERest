7711b9793ca098b76e4796dc1b0ff7c55c43a6f7
nishithakotakonda <nishitha.kotakonda@gmail.com>
2019-07-04 20:55:51 +0530

    Intial project

---
+++ CCLE-rest/src/main/java/com/ibank/card/offers/controller/CCLEController.java
@@ -0,0 +1,18 @@
+package com.ibank.card.offers.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cle")
+
+public class CCLEController {
+	
+	@RequestMapping(value="/healthCheck", method=RequestMethod.GET,produces="text/html")
+	
+	public String healthCheck(){
+		return "CCLE Service is working fine....";
+	}
+
+}
