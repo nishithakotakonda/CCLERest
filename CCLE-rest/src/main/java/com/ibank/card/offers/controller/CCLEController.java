97b2eb08e69c682945d096672b3c10562c18c5fb
nishithakotakonda <nishitha.kotakonda@gmail.com>
2019-07-05 20:32:42 +0530

    comment added

--- CCLE-rest/src/main/java/com/ibank/card/offers/controller/CCLEController.java
+++ CCLE-rest/src/main/java/com/ibank/card/offers/controller/CCLEController.java
@@ -3,7 +3,7 @@ package com.ibank.card.offers.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+//done with eclipse
 @RestController
 @RequestMapping("/cle")
 
